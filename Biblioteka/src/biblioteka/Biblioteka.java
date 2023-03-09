c4c1e05082f356b6ee2d39dab94ab99365482164
aleksa-radojicic <aleksa.radojicic@outlook.com>
2023-03-09 09:44:10 +0100

    Dodat komentar u klasu Biblioteka

--- Biblioteka/src/biblioteka/Biblioteka.java
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -9,6 +9,7 @@ public class Biblioteka implements BibliotekaInterfejs {
 
 	private List<Knjiga> knjige = new ArrayList<>();
 
+	//metoda koja dodaje knjigu u biblioteku
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
 		
